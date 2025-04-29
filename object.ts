bb2be2bebc2339bec9bc4e77cf8108b606d6e815
rushikeshalone <rushikeshalone.me@sbjit.edu.in>
2025-04-29 19:20:43 +0530

    Changes

--- object.ts
+++ object.ts
@@ -27,4 +27,20 @@ function getuserDetails(User: User): string {
   return `User name is ${User.name} and email is ${User.email} and isActive is ${User.isActive}`;
 }
 
+// Read only and optional properties
+type User1 = {
+  readonly _id: string;
+  name: string;
+  email: string;
+  isActive: boolean;
+  age?: number; // optional property
+};
+
+let myUserReadonly: User1 = {
+  _id: "123456",
+  name: "rushikesh",
+  email: "rushi@GMAIL.COM",
+  isActive: true,
+};
+
 export {};
